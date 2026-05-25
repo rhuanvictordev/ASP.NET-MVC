c9ecc19beaffdf730a4dce6a99832a7b376c1c27
Rhuan Oliveira <rhuan.oliveira@br.roca.com>
2026-05-25 13:58:54 -0300

    controller delete contato

--- Repositorio/ContatoRepositorio.cs
+++ Repositorio/ContatoRepositorio.cs
@@ -5,23 +5,59 @@ namespace MeuSiteEmMVC.Repositorio
 {
     public class ContatoRepositorio : IContatoRepositorio
     {
-        private readonly BancoContext _bancoContext;
+        private readonly BancoContext _context;
 
         public ContatoRepositorio(BancoContext bancoContext)
         {
-            _bancoContext = bancoContext;
+            _context = bancoContext;
         }
 
         public ContatoModel Adicionar(ContatoModel contato)
         {
-            _bancoContext.Contatos.Add(contato);
-            _bancoContext.SaveChanges();
+            _context.Contatos.Add(contato);
+            _context.SaveChanges();
             return contato;
+        }
+
+        public ContatoModel listarPorId(int id)
+        {
+            return _context.Contatos.FirstOrDefault(x => x.Id == id);
         }
 
         public List<ContatoModel> BuscarTodos()
         {
-            return _bancoContext.Contatos.ToList();
+            return _context.Contatos.ToList();
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDb = listarPorId(contato.Id);
+            if (contatoDb == null) 
+                throw new Exception("Houve um erro na atualização do contato!");
+
+            contatoDb.Nome = contato.Nome;
+            contatoDb.Email = contato.Email;
+            contatoDb.Celular = contato.Celular;
+            _context.Contatos.Update(contatoDb);
+            _context.SaveChanges();
+
+            return contatoDb;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDb = listarPorId(id);
+            if (contatoDb == null)
+                throw new Exception("Houve um erro na atualização do contato!");
+
+            _context.Contatos.Remove(contatoDb);
+            _context.SaveChanges();
+            return true;
+        }
+
+        ContatoModel IContatoRepositorio.Apagar(int id)
+        {
+            
         }
     }
 }
--- Repositorio/IContatoRepositorio.cs
+++ Repositorio/IContatoRepositorio.cs
@@ -4,7 +4,10 @@ namespace MeuSiteEmMVC.Repositorio
 {
     public interface IContatoRepositorio
     {
+        ContatoModel listarPorId(int id);
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
+        ContatoModel Apagar(int id);
     }
 }
